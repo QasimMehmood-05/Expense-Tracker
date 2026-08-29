47ca84601f4f5765e987964b15a5c57846563f7c
QasimMehmood-05 <qasim24729@gmail.com>
2026-08-29 12:32:56 +0500

    Made Header responisve

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
 
   return (
     <>
-      <div className='parent border rounded-2xl flex justify-between px-3 my-2 mx-2'>
+      <div className='parent border rounded-2xl flex justify-between items-center px-3 my-2 mx-0.5 md: text-sm  p-0'>
         <div className='lheader'>
-          <h2 className='text-2xl font-black'>Dashboard</h2>
+          <h2 className='text-3xl  md: font-medium'>Dashboard</h2>
           <p className='font-medium'>Welcome Back!</p>
         </div>
         <div className='Rheader mx-3 flex  justify-center items-center gap-3'>
-          <button className='bg-gray-400 rounded  p-2 flex items-center justify-center gap-x-2 hover:scale-105' > <FiRefreshCw />
+          <button className='bg-gray-400 rounded  p-1 flex items-center justify-center gap-x-2 hover:scale-105 md: py-1' > <FiRefreshCw />
             <span>Refersh</span></button>
-          <button onClick={() => { setshowmodal(true)  }} className='bg-blue-400 rounded  p-2 flex items-center justify-center gap-x-2 hover:scale-105' > <IoAdd /> <span>Add Transaction</span></button>
+          <button onClick={() => { setshowmodal(true)  }} className='bg-blue-400 rounded  mx-3 hover:scale-105 md: my-2 py-1.5 px-0 ' >  + Add Transaction</button>
         </div>
       </div>
 
