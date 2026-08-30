8ec1f7719f0d600370e78f1cae3d2b84aa0e7429
QasimMehmood-05 <qasim24729@gmail.com>
2026-08-30 16:16:56 +0500

    navbar changed

--- src/components/Transaction/TNav.jsx
+++ src/components/Transaction/TNav.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react'
-import { IoAdd } from "react-icons/io5";
 import Modalform from './Modalform';
 const TNav = () => {
     const [showModal, setShowModal] = useState(false);
     return (
         <>
 
-            <div className='nav flex justify-between m-2 p-2 '>
-                <div className="heading">
+            <div className='nav flex justify-between m-1 p-1 '>
+                <div className="heading px-2">
                     <h1 className='text-2xl font-black' >Transactions</h1>
                     <p className='text-sm font-light' >Manage your transactions here</p>
                 </div>
-                <div className="btn">
-                    <button onClick={() => setShowModal(true)} className='bg-blue-400 rounded  p-2 flex items-center justify-center gap-x-2 hover:scale-105' > <IoAdd /> <span>Add Transaction</span></button>
+                <div className="btn ">
+                    <button onClick={() => setShowModal(true)} className='bg-blue-400 rounded text-center  py-1  hover:scale-105' > + Add Transaction</button>
                 </div>
             </div>
             {showModal && (
